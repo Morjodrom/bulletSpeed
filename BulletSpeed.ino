491b08e9e72cc5f9301db8f0ce11b706182a4359
Dmitry <metaldr@yandex.ru>
2024-07-10 18:24:48 +0300

    Вывод полученных данных на дисплей

--- BulletSpeed.ino
+++ BulletSpeed.ino
@@ -1,19 +1,30 @@
 #include "Arduino.h"
+#include "GyverTM1637.h"
 
 #define PIN_SPEED_START 2
 #define PIN_SPEED_END 3
+#define TM1637_CLK 4
+#define TM1637_DIO 5
+
 #define DISTANCE_M 0.1
 
 #define DEBUG 1
 
+GyverTM1637 disp(TM1637_CLK, TM1637_DIO);
 
 volatile unsigned long interruptedStart = 0;
 volatile unsigned long interruptedEnd = 0;
 
 void setup()
 {
+    #ifdef DEBUG
     Serial.begin(9600);
     Serial.print("Start,end");
+    #endif
+
+    disp.clear();
+    disp.brightness(5);
+    disp.displayInt(0);
 
     pinMode(PIN_SPEED_START, INPUT);
     pinMode(PIN_SPEED_END, INPUT);
@@ -27,6 +38,7 @@ void loop()
     if(interruptedStart > 0 && interruptedEnd > 0){
         unsigned long diff = calculateTimeDifference();
         float speedMPerS = DISTANCE_M / (diff / 1000000.0f);
+        printSpeed(speedMPerS);
 
         #ifdef DEBUG
         Serial.println();
@@ -58,4 +70,23 @@ unsigned long calculateTimeDifference()
         return interruptedEnd - interruptedStart;
     }
     return interruptedStart - interruptedEnd;
+}
+
+void printSpeed(float speedMPerS)
+{
+    disp.clear();
+    
+    Serial.println("SpeedMPerS " + String(speedMPerS));
+
+    if(speedMPerS > 1){
+        disp.displayInt((int) speedMPerS);
+    } else {
+        int percent = floor(speedMPerS * 100);
+        // str pad
+        disp.displayInt(percent);
+        disp.display(1, 0);
+        if(percent < 10){
+            disp.display(2, 0);
+        }
+    }
 }
